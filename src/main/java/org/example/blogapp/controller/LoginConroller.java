83fbfca16c5736502bf40d34efaa22e6718cc4ad
Nathan Hacault <nathanhacault@gmail.com>
2023-11-24 18:33:04 -0500

    changed groupID

--- src/main/java/org/example/blogapp/controller/LoginConroller.java
+++ src/main/java/org/example/blogapp/controller/LoginConroller.java
@@ -1,4 +1,4 @@
-package org.martynas.blogapp.controller;
+package org.example.blogapp.controller;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
